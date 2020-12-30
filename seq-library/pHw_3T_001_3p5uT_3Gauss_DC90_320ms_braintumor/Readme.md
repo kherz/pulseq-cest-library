80da70170e3451700e1eb670ec52a9049cfe5bee
kherz <kai.herz@outlook.com>
2020-12-30 15:27:28 +0100

    phw has multiple slices

--- seq-library/pHw_3T_001_3p5uT_3Gauss_DC90_320ms_braintumor/Readme.md
+++ seq-library/pHw_3T_001_3p5uT_3Gauss_DC90_320ms_braintumor/Readme.md
@@ -1,5 +1,8 @@
 ## pHw_3T_001_3p5uT_3Gauss_DC90_320ms_braintumor
 
+This is a multi-slice method with repeating saturation between slices.
+There is an ADC event for each slice, which means, each frequency offset contains multiple ADC events.
+
 **NOT approved yet**
 
 
@@ -13,7 +16,7 @@ pH weighted protocol for 3T
 
 ### Publication
 
- Harris, RJ, Yao, J, Chakhoyan, A, et al. Simultaneous pH‚Äêsensitive and oxygen‚Äêsensitive MRI of human gliomas at 3 T using multi‚Äêecho amine proton chemical exchange saturation transfer spin‚Äêand‚Äêgradient echo echo‚Äêplanar imaging (CEST‚ÄêSAGE‚ÄêEPI). Magn Reson Med. 2018; 80: 1962‚Äì 1978. https://doi.org/10.1002/mrm.27204 
+ Harris, RJ, Yao, J, Chakhoyan, A, et al. Simultaneous pH‚Ä?sensitive and oxygen‚Ä?sensitive MRI of human gliomas at 3 T using multi‚Ä?echo amine proton chemical exchange saturation transfer spin‚Ä?and‚Ä?gradient echo echo‚Ä?planar imaging (CEST‚Ä?SAGE‚Ä?EPI). Magn Reson Med. 2018; 80: 1962‚Äì 1978. https://doi.org/10.1002/mrm.27204 
 
 ### Plot
 <img src="pHw_3T_001_3p5uT_3Gauss_DC90_320ms_braintumor.png" width="300"/> 
--- seq-library/pHw_3T_001_3p5uT_3Gauss_DC90_320ms_braintumor/pHw_3T_001_3p5uT_3Gauss_DC90_320ms_braintumor.m
+++ seq-library/pHw_3T_001_3p5uT_3Gauss_DC90_320ms_braintumor/pHw_3T_001_3p5uT_3Gauss_DC90_320ms_braintumor.m
@@ -2,6 +2,10 @@
 % Creates a sequence file for an ph weighted protocol according to
 % https://doi.org/10.1002/mrm.27204
 
+% This is a multi-slice method with repeating saturation between slices.
+% There is an ADC event for each slice, which means, each frequency offset
+% contains nSlices (25) ADC events
+
 % Kai Herz 2020
 % kai.herz@tuebingen.mpg.de
 
@@ -20,8 +24,6 @@ end
 seq_defs.n_pulses      = 3              ; % number of pulses
 seq_defs.tp            = 100e-3           ; % pulse duration [s]
 seq_defs.td            = 10e-3            ; % interpulse delay [s]
-seq_defs.Trec          = 10             ; % not found > 10000 ms
-seq_defs.Trec_M0       = 10             ; % not found > 10000 ms
 seq_defs.M0_offset     = []           ; % m0 offset [ppm]
 seq_defs.DCsat         = (seq_defs.tp)/(seq_defs.tp+seq_defs.td); % duty cycle
 seq_defs.offsets_ppm   = [-3.5:0.1:-2.5, -0.3:0.1:0.3, 2.5:0.1:3.5]; % ?3.5 to ?2.5 ppm, ?0.3 to +0.3 ppm, and +2.5 to +3.5 ppm, all in increments of 0.1 ppm
@@ -30,12 +32,11 @@ seq_defs.Tsat          = seq_defs.n_pulses*(seq_defs.tp+seq_defs.td) - ...
     seq_defs.td ;  % saturation time [s]
 seq_defs.B0            = 3               ; % B0 [T]
 seq_defs.seq_id_string = seqid           ; % unique seq id
+seq_defs.nSlices       = 25;  % 
 
 
 %% get info from struct
 offsets_ppm = seq_defs.offsets_ppm; % [ppm]
-Trec        = seq_defs.Trec;        % recovery time between scans [s]
-Trec_M0     = seq_defs.Trec_M0;     % recovery time before m0 scan [s]
 tp          = seq_defs.tp;          % sat pulse duration [s]
 td          = seq_defs.td;          % delay between pulses [s]
 n_pulses    = seq_defs.n_pulses;    % number of sat pulses per measurement. if DC changes use: n_pulses = round(2/(t_p+t_d))
@@ -74,32 +75,33 @@ offsets_Hz = offsets_ppm*gyroRatio_hz*B0;
 
 % init sequence
 seq = mr.Sequence();
-% m0 is unsaturated
-seq.addBlock(mr.makeDelay(Trec_M0));
-seq.addBlock(pseudoADC);
 
-% loop through offsets and set pulses and delays
-for currentOffset = offsets_Hz
-    
-    seq.addBlock(mr.makeDelay(Trec)); % recovery time
-    
-    satPulse.freqOffset = currentOffset; % set freuqncy offset of the pulse
-    accumPhase=0;
-    for np = 1:n_pulses
-        satPulse.phaseOffset = mod(accumPhase,2*pi); % set accumulated pahse from previous rf pulse
-        seq.addBlock(satPulse) % add sat pulse
-        % calc phase for next rf pulse
-        accumPhase = mod(accumPhase + currentOffset*2*pi*(numel(find(abs(satPulse.signal)>0))*1e-6),2*pi);
-        if np < n_pulses % delay between pulses
-            seq.addBlock(mr.makeDelay(td)); % add delay
-        end
-    end
-    if spoiling % spoiling before readout
-        seq.addBlock(gySpoil);
-    end
+% unsaturated m0
+for nSl = 1:seq_defs.nSlices
+    seq.addBlock(mr.makeDelay(seq_defs.Tsat));
+    seq.addBlock(gxSpoil,gySpoil,gzSpoil);
     seq.addBlock(pseudoADC); % readout trigger event
 end
 
+% loop through offsets and set pulses and delays
+for currentOffset = offsets_Hz
+    satPulse.freqOffset = currentOffset; % set freuqncy offset of the pulse
+    for nSl = 1:seq_defs.nSlices
+        accumPhase=0;
+        for np = 1:n_pulses
+            satPulse.phaseOffset = mod(accumPhase,2*pi); % set accumulated pahse from previous rf pulse
+            seq.addBlock(satPulse) % add sat pulse
+            % calc phase for next rf pulse
+            accumPhase = mod(accumPhase + currentOffset*2*pi*(numel(find(abs(satPulse.signal)>0))*1e-6),2*pi);
+            if np < n_pulses % delay between pulses
+                seq.addBlock(mr.makeDelay(td)); % add delay
+            end
+        end
+        seq.addBlock(gxSpoil,gySpoil,gzSpoil);
+        seq.addBlock(pseudoADC); % readout trigger event
+    end
+    
+end
 
 
 %% write definitions
@@ -109,11 +111,7 @@ for n_id = 1:numel(def_fields)
 end
 seq.write(seq_filename, author);
 
-%% plot
-save_seq_plot(seq_filename);
 
-% %% call standard sim
-% Simulate_and_plot_seq_file(seq_filename, B0);
 
 
 
