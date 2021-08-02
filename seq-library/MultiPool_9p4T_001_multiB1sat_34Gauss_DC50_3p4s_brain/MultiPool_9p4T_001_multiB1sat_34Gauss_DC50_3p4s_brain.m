b87171338f252652ba7242cba9d41eb23701393d
Felix Glang <felix.glang@tuebingen.mpg.de>
2021-08-02 13:18:54 +0200

    added new sequence for 9.4 T CEST parameter quantification by Bloch-McConnell fitting

---
+++ seq-library/MultiPool_9p4T_001_multiB1sat_34Gauss_DC50_3p4s_brain/MultiPool_9p4T_001_multiB1sat_34Gauss_DC50_3p4s_brain.m
@@ -0,0 +1,128 @@
+%% MultiPool_9p4T_001_multiB1sat_34Gauss_DC50_3p4s_brain
+% Creates a sequence file for an multi pool protocol for 9.4T according to
+% https://cest-sources.org/doku.php?id=standard_cest_protocols
+% MP_1 (//GUFI TH2//)
+
+%
+% Kai Herz 2020
+% kai.herz@tuebingen.mpg.de
+
+% author name for sequence file
+author = 'Felix Glang';
+
+%% get id of generation file
+if contains(mfilename, 'LiveEditorEvaluationHelperESectionEval')
+    [~, seqid] = fileparts(matlab.desktop.editor.getActiveFilename);
+else
+    [~, seqid] = fileparts(which(mfilename));
+end
+
+%% sequence definitions
+% everything in seq_defs gets written as definition in .seq-file
+seq_defs.n_pulses      = 34              ; % number of pulses
+seq_defs.tp            = 50e-3           ; % pulse duration [s]
+seq_defs.td            = 50e-3            ; % interpulse delay [s]
+seq_defs.Trec          = 3.5             ; % recovery time [s]
+seq_defs.Trec_M0       = 12             ; % recovery time before M0 [s]
+seq_defs.M0_offset     = -300           ; % m0 offset [ppm]
+seq_defs.DCsat         = (seq_defs.tp)/(seq_defs.tp+seq_defs.td); % duty cycle
+seq_defs.offsets_ppm   = [seq_defs.M0_offset -50.00 -30.00 -20.00 -12.00 -8.00 -6.00 -5.60...
+    -5.30 -5.00 -4.70 -4.40 -4.10 -3.80 -3.50 -3.20 -2.90 -2.60 -2.30 -2.10 -2.00 -1.70 ...
+    -1.40 -1.15 -1.00 -0.85 -0.75 -0.50 -0.20 0.00 0.20 0.50 0.60 0.70 0.80 1.00 1.15 1.30...
+    1.45 1.60 1.75 1.90 2.00 2.10 2.25 2.40 2.55 2.70 2.85 3.00 3.15 3.30 3.40 3.50 3.60 3.70...
+    3.85 4.00 4.15 4.30 4.45 4.60 4.75 4.90 5.00 5.15 5.30 5.45 6.25 8.00 12.00 20.00 30.00 50.00 ];  % offset vector [ppm]
+seq_defs.num_meas      = numel(seq_defs.offsets_ppm)   ; % number of repetition
+seq_defs.Tsat          = seq_defs.n_pulses*(seq_defs.tp+seq_defs.td) - ...
+    seq_defs.td ;  % saturation time [s]
+seq_defs.B0            = 9.4               ; % B0 [T]
+seq_defs.seq_id_string = seqid           ; % unique seq id
+
+
+%% get info from struct
+offsets_ppm = seq_defs.offsets_ppm; % [ppm]
+Trec        = seq_defs.Trec;        % recovery time between scans [s]
+Trec_M0     = seq_defs.Trec_M0;     % recovery time before m0 scan [s]
+tp          = seq_defs.tp;          % sat pulse duration [s]
+td          = seq_defs.td;          % delay between pulses [s]
+n_pulses    = seq_defs.n_pulses;    % number of sat pulses per measurement. if DC changes use: n_pulses = round(2/(t_p+t_d))
+B0          = seq_defs.B0;          % B0 [T]
+
+%% multiple saturation pulse amplitudes
+B1pa        = [0.6, 0.9, 1.1, 1.4];  % mean sat pulse b1 [uT]
+
+%%
+spoiling    = 1;     % 0=no spoiling, 1=before readout, Gradient in x,y,z
+seq_filename = strcat(seq_defs.seq_id_string,'.seq'); % filename
+
+%% scanner limits
+% see pulseq doc for more ino
+lims = Get_scanner_limits();
+
+%% create scanner events
+% satpulse
+gyroRatio_hz  = 42.5764;                  % for H [Hz/uT]
+gyroRatio_rad = gyroRatio_hz*2*pi;        % [rad/uT]
+
+% spoilers
+spoilRiseTime = 1e-3;
+spoilDuration = 4500e-6+ spoilRiseTime; % [s]
+% create pulseq gradient object
+[gxSpoil, gySpoil, gzSpoil] = Create_spoiler_gradients(lims, spoilDuration, spoilRiseTime);
+
+% pseudo adc, not played out
+pseudoADC = mr.makeAdc(1,'Duration', 1e-3);
+
+%% loop through sequence
+offsets_Hz = offsets_ppm*gyroRatio_hz*B0;
+
+% init sequence
+seq = mr.Sequence();
+
+for currentB1sat = B1pa % loop through B1sat's
+
+    fa_sat        = currentB1sat*gyroRatio_rad*tp; % flip angle of sat pulse
+    % create pulseq saturation pulse object
+    satPulse      = mr.makeGaussPulse(fa_sat, 'Duration', tp,'system',lims,'timeBwProduct', 0.2,'apodization', 0.5); % siemens-like gauss
+    [B1cwpe,B1cwae,B1cwae_pure,alpha]= calc_power_equivalents(satPulse,tp,td,1,gyroRatio_hz);
+    seq_defs.B1cwpe = B1cwpe;
+    
+    % loop through offsets and set pulses and delays
+    for currentOffset = offsets_Hz
+        if currentOffset == seq_defs.M0_offset*gyroRatio_hz*B0
+            seq.addBlock(mr.makeDelay(Trec_M0));
+        end
+        satPulse.freqOffset = currentOffset; % set freuqncy offset of the pulse
+        accumPhase=0;
+        for np = 1:n_pulses
+            satPulse.phaseOffset = mod(accumPhase,2*pi); % set accumulated pahse from previous rf pulse
+            seq.addBlock(satPulse) % add sat pulse
+            % calc phase for next rf pulse
+            accumPhase = mod(accumPhase + currentOffset*2*pi*(numel(find(abs(satPulse.signal)>0))*1e-6),2*pi);
+            if np < n_pulses % delay between pulses
+                seq.addBlock(mr.makeDelay(td)); % add delay
+            end
+        end
+        if spoiling % spoiling before readout
+            seq.addBlock(gxSpoil,gySpoil,gzSpoil);
+        end
+        seq.addBlock(pseudoADC); % readout trigger event
+    end
+    fprintf('a B1sat is done\n')
+end
+
+%% write definitions
+def_fields = fieldnames(seq_defs);
+for n_id = 1:numel(def_fields)
+    seq.setDefinition(def_fields{n_id}, seq_defs.(def_fields{n_id}));
+end
+seq.write(seq_filename, author);
+
+%% plot
+save_seq_plot(seq_filename);
+
+%% call standard sim
+% M_z = Run_pulseq_cest_Simulation(seq_filename,'../../sim-library/GM_3T_001_bmsim.yaml');
+
+
+
+
---
+++ seq-library/MultiPool_9p4T_001_multiB1sat_34Gauss_DC50_3p4s_brain/Readme.md
@@ -0,0 +1,19 @@
+## MultiPool_9p4T_001_multiB1sat_34Gauss_DC50_3p4s_brain
+
+### Description
+Multi-pool, multi-B1sat protocol for 9.4 T
+optimized for in vivo parameter quantification experiments by multi-pool Bloch-McConnell fitting
+
+* B<sub>1</sub> = 0.6, 0.9, 1.1, 1.4 µT 
+* T<sub>sat</sub> = 3.4*4 s
+* DC<sub>sat</sub> = 50 %
+
+### Publication
+
+Glang F. CEST MRI parameter estimation and uncertainty quantification in the human brain via deep learning approaches. Master's Thesis: University of Tuebingen & Max Planck Institute for Biological Cybernetics; 2019.
+
+
+
+### Plot
+
+
