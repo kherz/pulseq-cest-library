3b7a34bf2b1e8f2dd8115aebf9a3f5e645242521
kherz <kai.herz@outlook.com>
2022-07-14 14:01:47 +0200

    typo fix and python files

--- seq-library/MRF_CEST_MT_3T_003_13SL_DC50_2500ms/MRF_CEST_MT_3T_003_13SL_DC50_2500ms.m
+++ seq-library/MRF_CEST_MT_3T_003_13SL_DC50_2500ms/MRF_CEST_MT_3T_003_13SL_DC50_2500ms.m
@@ -1,6 +1,8 @@
-%% MRF_CEST_L-Arginine_3T_002_13SL_DC50_2500ms
+%% MRF_CEST_MT_3T_003_13SL_DC50_2500ms
 % Creates a sequence file for a MT CEST Fingerprinting protocol
-% Reference: tbd
+% Reference: Perlman, O., Ito, H., Herz, K. et al. 
+% Quantitative imaging of apoptosis following oncolytic virotherapy by magnetic resonance fingerprinting aided by deep learning. 
+% Nat. Biomed. Eng, 2022, https://doi.org/10.1038/s41551-021-00809-7
 %
 
 % author name for sequence file
