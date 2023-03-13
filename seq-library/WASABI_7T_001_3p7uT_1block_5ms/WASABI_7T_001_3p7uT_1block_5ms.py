c08a02b5761402d2837a89598fed770ddce3bb83
Patrick Schuenke <patrick.schuenke@gmail.com>
2023-03-13 11:25:38 +0100

    Update case WASABI_7T_001

--- seq-library/WASABI_7T_001_3p7uT_1block_5ms/WASABI_7T_001_3p7uT_1block_5ms.py
+++ seq-library/WASABI_7T_001_3p7uT_1block_5ms/WASABI_7T_001_3p7uT_1block_5ms.py
@@ -1,52 +1,66 @@
 # WASABI_7T_001_3p7uT_1block_5ms
-# Creates a sequence file for a WASABI protocol with 31 offsets and one M0 image at 7T according to:
-# https://doi.org/10.1002/mrm.26133
+# WASABI protocol with 31 offsets and one M0 image at 7T according to: https://doi.org/10.1002/mrm.26133
 #
-# Patrick Schuenke 2022
+# IMPORTANT INFO: if you get an AssertionError in the timing check, just comment out the corresponding assert statement:
+#       "assert abs(duration - block_duration) < eps"
+# in pypulseq\Sequence\block.py. This is a known bug in pypulseq 1.3.1post1.
+#
+# Tested with pypulseq version 1.3.1post1 and bmctool version 0.6.0
+#
+# Patrick Schuenke 2023
 # patrick.schuenke@ptb.de
 
-import os
+from pathlib import Path
 
 import numpy as np
+import pypulseq as pp
 from bmctool.utils.seq.write import write_seq
-from pypulseq import Opts
-from pypulseq import Sequence
-from pypulseq import make_adc
-from pypulseq import make_block_pulse
-from pypulseq import make_delay
-from pypulseq import make_trapezoid
 
 # get id of generation file
-seqid = os.path.splitext(os.path.basename(__file__))[0]
+seqid = Path(__file__).stem + "_python"
+
+# get folder of generation file
+folder = Path(__file__).parent
 
 # general settings
-author = 'Patrick Schuenke'
-plot_sequence = False  # plot preparation block?
-convert_to_1_3 = False  # convert seq-file to a version 1.3 file? Needed for pypulseq < v1.3.1 only!
-check_timing = True  # Perform a timing check at the end of the sequence
+AUTHOR = "Patrick Schuenke"
+FLAG_PLOT_SEQUENCE = False  # plot preparation block?
+FLAG_CHECK_TIMING = False  # perform a timing check at the end of the sequence?
+FLAG_POST_PREP_SPOIL = True  # add spoiler after preparation block?
 
-# sequence definitions (everything in seq_defs will be written to definitions of the .seq-file)
-seq_defs: dict = {}
-seq_defs['b1rms'] = 3.7  # B1 amplitude [µT]
-seq_defs['b0'] = 7  # B0 [T]
-seq_defs['n_pulses'] = 1  # number of pulses  #
-seq_defs['tp'] = 5e-3  # pulse duration [s]
-seq_defs['trec'] = 3  # recovery time [s]
-seq_defs['trec_m0'] = 12  # recovery time before M0 [s]
-seq_defs['m0_offset'] = -300  # m0 offset [ppm]
-seq_defs['offsets_ppm'] = np.append(seq_defs['m0_offset'], np.linspace(-1.5, 1.5, 31))  # offset vector [ppm]
+# sequence definitions
+defs: dict = {}
+defs["b1pa"] = 3.7  # B1 peak amplitude [µT]
+defs["b1rms"] = defs["b1pa"]  # B1 RMS amplitude [µT]
+defs["b0"] = 7  # B0 [T]
+defs["n_pulses"] = 1  # number of pulses  #
+defs["tp"] = 5e-3  # pulse duration [s]
+defs["trec"] = 3  # recovery time [s]
+defs["trec_m0"] = 12  # recovery time before M0 [s]
+defs["m0_offset"] = -300  # m0 offset [ppm]
+defs["offsets_ppm"] = np.append(defs["m0_offset"], np.linspace(-1.5, 1.5, 31))
 
-seq_defs['num_meas'] = seq_defs['offsets_ppm'].size  # number of repetition
-seq_defs['tsat'] = seq_defs['tp']  # saturation time [s]
-seq_defs['seq_id_string'] = seqid  # unique seq id
+defs["num_meas"] = defs["offsets_ppm"].size  # number of repetition
+defs["tsat"] = defs["tp"]  # saturation time [s]
+defs["seq_id_string"] = seqid  # unique seq id
+defs["spoiling"] = "1" if FLAG_POST_PREP_SPOIL else "0"
 
-seq_filename = seq_defs['seq_id_string'] + '.seq'
+seq_filename = defs["seq_id_string"] + ".seq"
 
 # scanner limits
-sys = Opts(max_grad=40, grad_unit='mT/m', max_slew=130, slew_unit='T/m/s',
-           rf_ringdown_time=30e-6, rf_dead_time=100e-6, rf_raster_time=1e-6)
+sys = pp.Opts(
+    max_grad=40,
+    grad_unit="mT/m",
+    max_slew=130,
+    slew_unit="T/m/s",
+    rf_ringdown_time=30e-6,
+    rf_dead_time=100e-6,
+    rf_raster_time=1e-6,
+    gamma=42576400,
+)
 
-gamma_hz =seq.sys.gamma*1e-6
+GAMMA_HZ = sys.gamma * 1e-6
+defs["freq"] = defs["b0"] * GAMMA_HZ  # Larmor frequency [Hz]
 
 # ===========
 # PREPARATION
@@ -57,63 +71,63 @@ spoil_amp = 0.8 * sys.max_grad  # Hz/m
 rise_time = 1.0e-3  # spoiler rise time in seconds
 spoil_dur = 6.5e-3  # complete spoiler duration in seconds
 
-gx_spoil, gy_spoil, gz_spoil = [make_trapezoid(channel=c, system=sys, amplitude=spoil_amp, duration=spoil_dur,
-                                               rise_time=rise_time) for c in ['x', 'y', 'z']]
+gx_spoil, gy_spoil, gz_spoil = [
+    pp.make_trapezoid(channel=c, system=sys, amplitude=spoil_amp, duration=spoil_dur, rise_time=rise_time)
+    for c in ["x", "y", "z"]
+]
 
 # RF pulses
-flip_angle_sat = seq_defs['b1rms'] * gamma_hz * 2 * np.pi * seq_defs['tp']
-rf_pulse = make_block_pulse(flip_angle=flip_angle_sat, duration=seq_defs['tp'], system=sys)
+flip_angle_sat = defs["b1pa"] * GAMMA_HZ * 2 * np.pi * defs["tp"]
+sat_pulse = pp.make_block_pulse(flip_angle=flip_angle_sat, duration=defs["tp"], system=sys)
 
-# ADC events
-pseudo_adc = make_adc(num_samples=1, duration=1e-3)  # (not played out; just used to split measurements)
+# pseudo ADC event
+pseudo_adc = pp.make_adc(num_samples=1, duration=1e-3)
 
-# DELAYS
-trec_delay = make_delay(seq_defs['trec'])
-m0_delay = make_delay(seq_defs['trec_m0'])
+# delays
+trec_delay = pp.make_delay(defs["trec"])
+m0_delay = pp.make_delay(defs["trec_m0"])
 
 # Sequence object
-seq = Sequence()
+seq = pp.Sequence()
 
 # ===
 # RUN
 # ===
 
-offsets_hz = seq_defs['offsets_ppm'] * gamma_hz * seq_defs['b0']  # convert from ppm to Hz
+offsets_hz = defs["offsets_ppm"] * defs["freq"]  # convert from ppm to Hz
 
 for m, offset in enumerate(offsets_hz):
     # print progress/offset
-    print(f' {m + 1} / {len(offsets_hz)} : offset {offset}')
+    print(f"#{m + 1} / {len(offsets_hz)} : offset {offset / defs['freq']:.2f} ppm ({offset:.3f} Hz)")
 
     # add delay
-    if offset == seq_defs['m0_offset'] * gamma_hz * seq_defs['b0']:
-        if seq_defs['trec_m0'] > 0:
+    if offset == defs["m0_offset"] * defs["freq"]:
+        if defs["trec_m0"] > 0:
             seq.add_block(m0_delay)
     else:
-        if seq_defs['trec'] > 0:
+        if defs["trec"] > 0:
             seq.add_block(trec_delay)
 
     # set wasabi pulse
-    rf_pulse.freq_offset = offset
-    seq.add_block(rf_pulse)
+    sat_pulse.freq_offset = offset
+    seq.add_block(sat_pulse)
 
-    seq.add_block(gx_spoil, gy_spoil, gz_spoil)
+    if FLAG_POST_PREP_SPOIL:
+        seq.add_block(gx_spoil, gy_spoil, gz_spoil)
+
+    # add pseudo ADC event
     seq.add_block(pseudo_adc)
 
-if check_timing:
+if FLAG_CHECK_TIMING:
     ok, error_report = seq.check_timing()
     if ok:
-        print('\nTiming check passed successfully')
+        print("\nTiming check passed successfully")
     else:
-        print('\nTiming check failed! Error listing follows\n')
+        print("\nTiming check failed! Error listing follows\n")
         print(error_report)
 
-write_seq(seq=seq,
-          seq_defs=seq_defs,
-          filename=seq_filename,
-          author=author,
-          use_matlab_names=True,
-          convert_to_1_3=convert_to_1_3)
+write_seq(seq=seq, seq_defs=defs, filename=folder / seq_filename, author=AUTHOR, use_matlab_names=True)
 
 # plot the sequence
-if plot_sequence:
+if FLAG_PLOT_SEQUENCE:
     seq.plot()  # to plot all offsets, remove time_range argument
