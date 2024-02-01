a10e9a122012a32b5f353a04231cd7ebbe31307b
mzaiss <moritz.zaiss@uk-erlangen.de>
2024-02-01 17:38:04 +0100

    created a first generation script

--- eval-examples/WASABIT1/EVAL_WASABIT1.m
+++ eval-examples/WASABIT1/EVAL_WASABIT1.m
@@ -1,7 +1,7 @@
 %% EVAL WASABIT1
 % The following flag determines, if you want to operate on real data or
 % simulate the data
-data_flag= 'real_data'; % simulation, re_simulation or real_data
+data_flag= 're_simulation'; % simulation, re_simulation or real_data
 %% 1) Build up Filename, Structure, Paths
 
 %get the user specific pulseq path
@@ -26,8 +26,9 @@ switch data_flag
         M_z = load([seq_file_folder_path filesep 'M_z_' seq_filename '.txt']);
     case 're_simulation'
         %% 2b)  re-simulate
-        M_z = simulate_pulseqcest(seq_filename, [pulseq_path filesep 'sim-library' filesep 'WM_3T_default_7pool_bmsim.yaml']);
+        M_z = simulate_pulseqcest(seq_file_path, [pulseq_path filesep 'sim-library' filesep 'WM_3T_default_7pool_bmsim.yaml']);
         M_z=M_z';
+       
     case 'real_data'
         %% 2c)  read data from measurement (dicom)
         dcmpath=uigetdir('','Go to DICOM Directory'); cd(dcmpath)
@@ -53,24 +54,24 @@ switch data_flag
         M_z=V_M_z(:,maskInd);
 end
 
-        %% 3) Evaluation
-        % Normalization
-        Z_wasabi=double(M_z(2:end,:))./double(M_z(1,:));
+%% 3) Evaluation
+% Normalization
+Z_wasabi=double(M_z(2:end,:))./double(M_z(1,:));
 
-        % Fitting Parameters 
-        B1=seq.definitions('b1cwpe');
-        freq=seq.definitions('freq');
-        w=defs.offsets_ppm(2:end);
-        t_p=seq.definitions('tp');
-        t_rec=seq.definitions('trec');
-        fitoptions=[1E-04, 1E-15, 1E-10, 1E-4, 1E-06];
-        iterations = 100;
+% Fitting Parameters 
+B1=seq.definitions('B1pa');
+freq=seq.definitions('FREQ');
+w=defs.offsets_ppm;
+t_p=seq.definitions('tp');
+t_rec=seq.definitions('Trec');
+fitoptions=[1E-04, 1E-15, 1E-10, 1E-4, 1E-06];
+iterations = 100;
 
 %compute w and trec array and name it wt
-wt=cat(2,w,t_rec(2:end)); %in trec, account for 1st offset being M_0
+wt=cat(2,w(2:end)*freq,t_rec(2:end)); %in trec, account for 1st offset being M_0
 
 % Compute the analytic function of WASABIT1 curve
-%p(1:6)=
+% p(1) = B1, p(2) = dB0, p(3)=c, p(4) =d, p(5) T1 ;  [3.7, -0.1, 1, 2, 1.5];    
 wasabi_fit_2abs = @(p,w_and_trec_array) abs(1- exp(-w_and_trec_array(:,2)./p(5)) .* (p(3) - p(4) .* (pi * p(1) * gamma_hz * t_p).^2 .* (sinc(t_p .* sqrt((p(1) .* gamma_hz).^2 + (w_and_trec_array(:,1)-p(2)).^2))).^2));
 
 %wasabi_fit_2abs = @(p,w) abs(p(3) - p(4) .* sin(atan((p(1) ./ (freq / gamma_hz)) ./ (w - p(2)))).^2 .* sin(sqrt((p(1) ./ (freq / gamma_hz)).^2 + (w - p(2)).^2) .* freq .* (2 * pi) .* t_p / 2).^2);
@@ -83,17 +84,17 @@ Z=Z_wasabi;
 %% Perform the WASABI fit
 dB0_stack = zeros(1, size(Z, 2));
 rB1_stack = zeros(1, size(Z, 2));
-Z_fit = zeros(size(Z, 2), numel(w));
+Z_fit = zeros(size(Z, 2), numel(w)-1);
 T1_stack = zeros(1, size(Z, 2));
 
 for ii = 1:size(Z, 2)
-    if all(isfinite(Z(:, ii))) && Segment_resh(ii)==1
+    if all(isfinite(Z(:, ii))) 
         try
-            p0 = [3.7, -0.1, 1, 2, 1500];                              % initial Starting values                 
+            p0 = [3.7, -0.1, 1, -2, 1.8];                              % initial Starting values                 
             opts = optimset('Display','off');                        
             [p, ~] = lsqcurvefit(wasabi_fit_2abs,p0,wt,(Z(:,ii)),[],[],opts); 
             rB1_stack(ii) = p(1) / B1;
-            dB0_stack(ii) = p(2);
+            dB0_stack(ii) = p(2)/freq;
             T1_stack(ii)=p(5);
             Z_fit(ii,:) = wasabi_fit_2abs(p,wt);
   
@@ -106,7 +107,13 @@ for ii = 1:size(Z, 2)
     end
 end
 
+figure('Name',data_flag);
+subplot(1,2,1); plot(mean(Z,2),'rx-', 'DisplayName', 'Measurement'); title('Mean MTRasym-spectrum'); xlim([0 Inf]);set(gca,'Xdir','reverse'); hold on;
+subplot(1,2,1); plot(mean(Z_fit',2),'k-', 'DisplayName', 'Measurement'); title('Mean Z-spectrum'); set(gca,'Xdir','reverse');
 
+p
+
+if size(Z,2)>1
 %Vectorization Backwards
   sizes=size(V);
   B1=reshape(rB1_stack,[sizes(1) sizes(2) sizes(3)]);
@@ -118,3 +125,5 @@ end
  figure;
  subplot(1,2,1);imagesc(B0(:,:,10),[-0.2 0.2]);colorbar;title('B0 Map'); axis image
  subplot(1,2,2);imagesc(B1(:,:,10),[0.8 1.2]);colorbar;title('B1 Map'); axis image
+
+end
