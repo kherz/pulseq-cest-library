875f4b9743d3dce4eb5a986b2cafe66eb27197fd
Kemnas <moritz.fabian@fau.de>
2024-02-01 15:12:12 +0100

    the fitting fucntion works, but the starting parameters (as expected) are way off compared to using a WASABI bib. But maybe this will be solved with a neural network (MZ)

--- eval-examples/WASABIT1/EVAL_WASABIT1.m
+++ eval-examples/WASABIT1/EVAL_WASABIT1.m
@@ -7,7 +7,7 @@ data_flag= 'real_data'; % simulation, re_simulation or real_data
 %get the user specific pulseq path
 pulseq_struct=what('pulseq-cest-library');                  % look in Matlabpath if Folder is already included
 pulseq_path=pulseq_struct.path;
-seq_filename='WASABI_3T_001_3p7uT_1block_5ms.seq';
+seq_filename='WASABIT1_3T_001_3p7uT_1block_5p12ms.seq';
 seq_file_folder_path= [pulseq_path filesep 'seq-library' filesep extractBefore(seq_filename, '.seq')];
 seq_file_path= [seq_file_folder_path filesep seq_filename];
 
@@ -66,19 +66,12 @@ end
         fitoptions=[1E-04, 1E-15, 1E-10, 1E-4, 1E-06];
         iterations = 100;
 
-        c=1;
-        d=2;
+%compute w and trec array and name it wt
+wt=cat(2,w,t_rec(2:end)); %in trec, account for 1st offset being M_0
 
-Mzi = 1.0 - exp(-1.0  * trec / t1);
 % Compute the analytic function of WASABIT1 curve
 %p(1:6)=
-wasabi_fit_2abs = @(p,w) abs( ...
-                                1- exp(-/p(5))) * ((p(3) - p(4)* ( ...
-                                    pi * B1 * gamma_hz * t_p)^2) ...
-                                * sinc( ...
-                                            t_p * sqrt(...
-                                                (B1 * gamma_Hz)^2 + (w-p(2))^2)...
-                                ))^2
+wasabi_fit_2abs = @(p,w_and_trec_array) abs(1- exp(-w_and_trec_array(:,2)./p(5)) .* (p(3) - p(4) .* (pi * p(1) * gamma_hz * t_p).^2 .* (sinc(t_p .* sqrt((p(1) .* gamma_hz).^2 + (w_and_trec_array(:,1)-p(2)).^2))).^2));
 
 %wasabi_fit_2abs = @(p,w) abs(p(3) - p(4) .* sin(atan((p(1) ./ (freq / gamma_hz)) ./ (w - p(2)))).^2 .* sin(sqrt((p(1) ./ (freq / gamma_hz)).^2 + (w - p(2)).^2) .* freq .* (2 * pi) .* t_p / 2).^2);
 Z=Z_wasabi;
@@ -87,20 +80,22 @@ Z=Z_wasabi;
 % This anonymous function captures B1, freq, gamma_, and t_p from the outer scope
 %wasabi_fit_function = @(params, offset) wasabi_fit_2abs(params, offset);
 
-% Perform the WASABI fit
+%% Perform the WASABI fit
 dB0_stack = zeros(1, size(Z, 2));
 rB1_stack = zeros(1, size(Z, 2));
 Z_fit = zeros(size(Z, 2), numel(w));
+T1_stack = zeros(1, size(Z, 2));
 
 for ii = 1:size(Z, 2)
     if all(isfinite(Z(:, ii))) && Segment_resh(ii)==1
         try
             p0 = [3.7, -0.1, 1, 2, 1500];                              % initial Starting values                 
             opts = optimset('Display','off');                        
-            [p, ~] = lsqcurvefit(wasabi_fit_2abs,p0,w,(Z(:,ii)),[],[],opts); 
+            [p, ~] = lsqcurvefit(wasabi_fit_2abs,p0,wt,(Z(:,ii)),[],[],opts); 
             rB1_stack(ii) = p(1) / B1;
             dB0_stack(ii) = p(2);
-            Z_fit(ii,:) = wasabi_fit_2abs(p,w);
+            T1_stack(ii)=p(5);
+            Z_fit(ii,:) = wasabi_fit_2abs(p,wt);
   
         catch
             disp('something went wrong');
@@ -116,9 +111,10 @@ end
   sizes=size(V);
   B1=reshape(rB1_stack,[sizes(1) sizes(2) sizes(3)]);
   B0=reshape(dB0_stack,[sizes(1) sizes(2) sizes(3)]);
+  T1=reshape(T1_stack,[sizes(1) sizes(2) sizes(3)]);
   Zfit=reshape(Z_fit,[sizes(1) sizes(2) sizes(3) 31]);
 %% 4) Imaging
  % Display B1 and B0
  figure;
- subplot(1,2,1);imagesc(B0(:,:,6),[-0.2 0.2]);colorbar;title('B0 Map'); axis image
- subplot(1,2,2);imagesc(B1(:,:,6),[0.8 1.2]);colorbar;title('B1 Map'); axis image
+ subplot(1,2,1);imagesc(B0(:,:,10),[-0.2 0.2]);colorbar;title('B0 Map'); axis image
+ subplot(1,2,2);imagesc(B1(:,:,10),[0.8 1.2]);colorbar;title('B1 Map'); axis image
