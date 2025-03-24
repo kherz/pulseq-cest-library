c7801c81589fb7ba87bbed6d7ad65f0373c2ffeb
alex-liebeskind <63989601+alex-liebeskind@users.noreply.github.com>
2025-03-24 13:57:16 -0400

    Delete MAGMA_PAPER_2025 directory

--- MAGMA_PAPER_2025/Figure6.py
+++
@@ -1,9 +0,0 @@
-# Figure 6
-
-import sys
-from pathlib import Path
-
-original_path = sys.path.copy()
-
-from WM_comparison_parametrized import compare_WM
-compare_WM()
--- MAGMA_PAPER_2025/FigureS2.py
+++
@@ -1,3 +0,0 @@
-# Figure S2
-
-# Note: This figure is not code generated. The source is cited in the paper supplementary materials.
