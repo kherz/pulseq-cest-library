b88151ac9f549ecd6f3b0f0fb48f38acc845ae48
alex-liebeskind <63989601+alex-liebeskind@users.noreply.github.com>
2025-03-29 12:29:34 -0400

    Update Figure5.py

--- eval-examples/2025_MAGMA_PAPER/Figure5.py
+++ eval-examples/2025_MAGMA_PAPER/Figure5.py
@@ -8,7 +8,7 @@ original_path = sys.path.copy()
 subfigure = input('Please specify which subfigure to generate: ')
 
 if subfigure == 'a':
-  module_path = Path("../seq-library/WASABI_3T_001_3p7uT_1block_5ms").resolve()
+  module_path = Path("../../seq-library/WASABI_3T_001_3p7uT_1block_5ms").resolve()
   original_sys_path = sys.path.copy()
   sys.path.append(str(module_path))
   from WASABI_3T_001_3p7uT_1block_5ms_parametrized import generate_WASABI_sequence
@@ -27,7 +27,7 @@ elif subfigure == 'b' or subfigure == 'c' or subfigure == 'd' or subfigure == 'e
   # ROI y-minimum: 95
   # ROI y-maximum: 100
 
-  module_path = Path("../eval-examples/Wasabi").resolve()
+  module_path = Path("../Wasabi").resolve()
   original_sys_path = sys.path.copy()
   sys.path.append(str(module_path))
   from WASABI_3T_001_3p7uT_1block_5ms_Eval_parametrized import eval_WASABI
@@ -35,7 +35,18 @@ elif subfigure == 'b' or subfigure == 'c' or subfigure == 'd' or subfigure == 'e
   sys.path = original_sys_path
   
 elif subfigure == 'f' or subfigure == 'g':
-  module_path = Path("../eval-examples/Wasabi").resolve()
+  # Specifications:
+
+  # Data source: PULSEQ_HYBRID_GRE_2_2_5_WASABI_RR_0016
+  # 
+  # Do you want to specify an ROI [y/n]: y
+  # Slice of interest: 5
+  # ROI x-minimum: 145
+  # ROI x-maximum: 150
+  # ROI y-minimum: 95
+  # ROI y-maximum: 100
+  
+  module_path = Path("../Wasabi").resolve()
   original_sys_path = sys.path.copy()
   sys.path.append(str(module_path))
   from WASABI_3T_001_3p7uT_1block_5ms_Eval_parametrized import eval_WASABI
